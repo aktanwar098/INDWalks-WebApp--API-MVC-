9a1465f3c9aa45dcb0f0a95f87924f06e06e142f
aktanwar098 <ak.tanwar098@gmail.com>
2024-04-17 10:11:23 +0530

    Add project files.

---
+++ INDWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿namespace INDWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+
+        //Navigation Properties - When we want to add the other columns of a table to a single 
+        //we need to show its navigation by creating both class reference 
+
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+
+    }
+}
