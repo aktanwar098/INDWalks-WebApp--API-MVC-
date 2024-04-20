70933c443bc947d092f595c3aa0db593342aa148
aktanwar098 <ak.tanwar098@gmail.com>
2024-04-20 10:03:40 +0530

    Filtering

--- INDWalks.API/Controllers/WalksController.cs
+++ INDWalks.API/Controllers/WalksController.cs
@@ -41,10 +41,11 @@ namespace INDWalks.API.Controllers
 
         // GET All Walks
         // GET: /api/walks
+        // GET: /api/walks? filterOn = Name & filterQuery = Track
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn , [FromQuery] string? filterQuery)
         {
-            var walksDomainModel = await walkRepository.GetAllAsync();
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn , filterQuery);
 
             // Map Domain Model to Walk Dto and Return
 
