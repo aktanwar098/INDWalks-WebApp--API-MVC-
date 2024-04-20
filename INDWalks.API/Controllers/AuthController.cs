16b94283f44324dc032f3a6e98fce8b52b45143a
aktanwar098 <ak.tanwar098@gmail.com>
2024-04-20 21:17:04 +0530

    Created Auth Controller and Register action method

---
+++ INDWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using INDWalks.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INDWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        //POST : /api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (identityResult.Succeeded) 
+            {
+                //Add Role to this user
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any()) 
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if(identityResult.Succeeded) 
+                    {
+                        return Ok("User Registered Successfully!!.Please Login Now");
+                    }
+                }
+            }
+            return BadRequest("Something Went Wrong");
+
+        }
+    }
+}
