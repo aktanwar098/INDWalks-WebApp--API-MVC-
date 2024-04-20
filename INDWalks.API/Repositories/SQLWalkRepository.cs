b97e24784622c358215dd7fa6f0032e8c3912520
aktanwar098 <ak.tanwar098@gmail.com>
2024-04-20 11:02:42 +0530

    Pagination implementaion

--- INDWalks.API/Repositories/SQLWalkRepository.cs
+++ INDWalks.API/Repositories/SQLWalkRepository.cs
@@ -32,7 +32,7 @@ namespace INDWalks.API.Repositories
             return existingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null , string? sortBy = null, bool isAscending = true)
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null , string? sortBy = null, bool isAscending = true , int pageNumber = 1, int pageSize = 1000)
         {
             var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
 
@@ -59,7 +59,10 @@ namespace INDWalks.API.Repositories
                 }
             }
 
-            return await walks.ToListAsync();
+            //Pagination
+            var skipResults = (pageNumber - 1) * pageSize;
+
+            return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
         }
 
         public async Task<Walk?> GetByIdAsync(Guid id)
